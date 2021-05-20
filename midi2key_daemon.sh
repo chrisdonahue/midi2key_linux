d4796650d7ec4837926583bd27745e8ffb716773
Chris Donahue <christopherdonahue@gmail.com>
2021-05-20 13:21:58 -0700

    Added source

--- midi2key_daemon.sh
+++ midi2key_daemon.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# NOTE: Modified from https://superuser.com/questions/1170136/translating-midi-input-into-computer-keystrokes-on-linux
 aseqdump -p "Scarlett" | \
 while IFS=" ," read src ev1 ev2 ch label1 data1 label2 data2 rest; do
     case "$ev1 $ev2 $ch $data1 $data2" in

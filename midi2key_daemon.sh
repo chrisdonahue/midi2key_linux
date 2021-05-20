813f49d8e74ae696f4745dd01ea54f29274226d4
Chris Donahue <christopherdonahue@gmail.com>
2021-05-20 13:39:55 -0700

    Sends keyup/down to toggle instead of push-to-talk

--- midi2key_daemon.sh
+++ midi2key_daemon.sh
@@ -3,7 +3,7 @@
 aseqdump -p "Scarlett" | \
 while IFS=" ," read src ev1 ev2 ch label1 data1 label2 data2 rest; do
     case "$ev1 $ev2 $ch $data1 $data2" in
-        "Control change 0 64 127" ) xdotool keydown alt+a ;;
-        "Control change 1 64 0" ) xdotool keyup alt+a ;;
+        "Control change 0 64 127" ) xdotool key alt+a ;;
+        "Control change 1 64 0" ) xdotool key alt+a ;;
     esac
 done
